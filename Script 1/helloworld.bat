d66282b5aa5eb87dc334c620f48c732cb08ede62
PPinonFire <134116185+PPinonFire@users.noreply.github.com>
2024-05-18 12:39:28 -0600

    Add files via upload

---
+++ Script 1/helloworld.bat
@@ -0,0 +1,17 @@
+@echo off
+color A 
+echo Hello World
+echo.
+echo Listando del contenido en la carpeta actual:
+dir
+echo.
+echo Creando el la carpeta Test:
+mkdir Test
+echo.
+echo Entrando a la carpeta Test:
+cd Test
+echo.
+echo Listando del contenido en la carpeta Test:
+dir
+echo.
+pause
